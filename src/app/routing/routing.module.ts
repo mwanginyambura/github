27b3d153596b3e1bed809cd225a44c0f1e79167c
Nyambura Mwangi <mwanginyambura19@gmail.com>
2019-04-01 13:55:03 +0300

    factored out a file

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RepoComponent } from '../repo/repo.component';
 import { UserComponent } from '../user/user.component';
-import { NotFoundComponent } from '../not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
 
 
